88f1ba6d879cc906bcecf48bbca32d0c5a7fc1b3
Gabriel Botareli <gbotareli@gmail.com>
2021-05-29 16:03:00 -0300

    correção de sintaxe

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -14,6 +14,6 @@ def generate_unique_code():
 class Room(models.Model):
     code = models.CharField(max_length=8, default='', unique=True)
     host = models.CharField(max_length=50, default='', unique=True)
-    gest_can_pause = models.BooleanField(null=False, default=False)
+    guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
