afa672b3dc8d792e526da747d7bd089405d95546
Gabriel Botareli <gbotareli@gmail.com>
2021-05-31 00:10:03 -0300

    Inicio da criação da homepage

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -9,7 +9,10 @@ export default class App extends Component {
 
     render = () => {
         return (
-            <div>
+            <div style={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
                 <HomePage/>
             </div>
         )
--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react"
 import axios from "../utils/axios"
-import Button from "@material-ui/core/Button"
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import TextField from "@material-ui/core/TextField"
-import FormHelperText from "@material-ui/core/FormHelperText"
-import FormControl from "@material-ui/core/FormControl"
+import { Button, Grid, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from "@material-ui/core"
 import { Link } from "react-router-dom"
-import Radio from "@material-ui/core/Radio"
-import RadioGroup from "@material-ui/core/RadioGroup"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import { FormLabel } from "@material-ui/core"
 
 export default class CreateRoomPage extends Component {
     defaultVotes = 2
